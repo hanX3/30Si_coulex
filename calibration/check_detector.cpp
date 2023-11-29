0014a1194eaaca7bc5eef4e595874768c587d414
hanX3 <hanxingchi@gmail.com>
2023-11-29 10:42:15 +0800

    add

---
+++ calibration/check_detector.cpp
@@ -0,0 +1,30 @@
+
+void check_detector(int run, int mod)
+{
+  TString file_name;
+  file_name = Form("./get_his_raw/run%05d_raw_spe.root", run);
+  TFile *fi = TFile::Open(file_name.Data());
+  if(fi->IsZombie()){
+    cout << "can not open the file." << endl;
+    return ;
+  }
+
+  stringstream ss;
+  ss.str("");
+  ss << "mod" << mod;
+  gFile->cd(ss.str().c_str());
+
+  TCanvas *cc = new TCanvas("cc", "cc", 0, 0, 1240, 660);
+  cc->Divide(4, 3);
+
+  TH1D *h[12];
+  for(int i=0;i<12;i++){
+    cout << "drawing " << TString::Format("h_raw_mod%d_ch%d", mod, i) << endl;
+    h[i] = (TH1D*)gDirectory->Get(TString::Format("h_raw_mod%d_ch%d", mod, i).Data());
+
+    cc->cd(i+1);
+    cc->SetLogy();
+    h[i]->GetXaxis()->SetRangeUser(0., 4000.);
+    h[i]->Draw();
+  }
+}
