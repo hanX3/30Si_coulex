26b3264f7175d1831bf5dc9a655f18004d59e832
hanX3 <hanxingchi@gmail.com>
2023-11-29 12:36:04 +0800

    add

---
+++ ana_tmpfile_hanX/doppler/batch_analysis.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+# phi
+# nohup ./analysis.sh 1 0 0 0 0 & 
+# nohup ./analysis.sh 2 5.625 0 0 0 & 
+# nohup ./analysis.sh 3 11.25 0 0 0 & 
+# nohup ./analysis.sh 4 -5.625 0 0 0 & 
+# nohup ./analysis.sh 5 -11.25 0 0 0 & 
+
+# nohup ./analysis.sh 6 1 0 0 0 & 
+# nohup ./analysis.sh 7 2 0 0 0 & 
+# nohup ./analysis.sh 8 3 0 0 0 & 
+# nohup ./analysis.sh 9 4 0 0 0 & 
+# nohup ./analysis.sh 10 5 0 0 0 & 
+
+# nohup ./analysis.sh 11 0 0 0 0 & 
