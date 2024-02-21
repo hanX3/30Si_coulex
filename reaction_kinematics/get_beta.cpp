4c5da7147cf3ca2af158b73c6181c2019f84356f
hanX3 <hanxingchi@gmail.com>
2024-02-21 16:48:55 +0800

    add

--- reaction_kinematics/get_beta.cpp
+++ reaction_kinematics/get_beta.cpp
@@ -1,8 +1,8 @@
 //
-void get_si_beta()
+void get_beta_si()
 {
   ifstream fi;
-  fi.open("output.txt");
+  fi.open("output_si.txt");
   if(!fi){
     cout << "can not open the file" << endl;
     return;
@@ -21,4 +21,38 @@ void get_si_beta()
 
   fo.close();
   fi.close();
+}
+
+//
+void get_beta(_pt)
+{
+  ifstream fi1;
+  fi1.open("input_pt.txt");
+  if(!fi1){
+    cout << "can not open the file" << endl;
+    return;
+  }
+
+  ifstream fi2;
+  fi2.open("output_pt.txt");
+  if(!fi2){
+    cout << "can not open the file" << endl;
+    return;
+  }
+
+  ofstream fo;
+  fo.open("theta_beta_pt.txt");
+  double angle, e, beta[24];
+  for(int i=0;i<24;i++){
+    fi1 >> angle >> e;
+    fi2 >> e;
+    beta[i] = sqrt(e*2./931.5/195.964954675);
+    cout << "angle " << angle << " beta " << beta[i] << endl;
+    fo << angle << " " << beta[i] << endl;
+  }
+  fo << endl;
+
+  fo.close();
+  fi1.close();
+  fi2.close();
 }
