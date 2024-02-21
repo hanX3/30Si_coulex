3852bb1f1986478a0e67465b5a0e150760cfc4a2
hanX3 <hanxingchi@gmail.com>
2024-02-21 16:30:30 +0800

    add

--- reaction_kinematics/batch_elast_pt.sh
+++ reaction_kinematics/batch_elast_pt.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 
-if [ -e "output_si.txt" ];then
-  rm output_si.txt
+if [ -e "output_pt.txt" ];then
+  rm output_pt.txt
 fi
 
 while IFS= read -r line; do
   angle=$(echo "$line" | awk '{print $1}')
   energy=$(echo "$line" | awk '{print $2}')
-  elast -Qa $angle o "1(196Pt)" 1.3 30Si $energy >> output_si.txt
+  elast -Qa $angle o "1(196Pt)" 1.3 196Pt $energy >> output_pt.txt
   # echo $angle 
   # echo $energy
-done < input_si.txt 
+done < input_pt.txt 
 
