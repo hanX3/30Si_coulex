2f43909f47d7ecfb7b43699cd73817d0dabf3f3d
hanX3 <hanxingchi@gmail.com>
2024-02-22 11:54:32 +0800

    add

--- reaction_kinematics/batch_elast_pt.sh
+++ reaction_kinematics/batch_elast_pt.sh
@@ -7,7 +7,7 @@ fi
 while IFS= read -r line; do
   angle=$(echo "$line" | awk '{print $1}')
   energy=$(echo "$line" | awk '{print $2}')
-  elast -Qa $angle o "1(196Pt)" 1.3 196Pt $energy >> output_pt.txt
+  elast -Qa $angle o "1(196Pt)" 0.05 196Pt $energy >> output_pt.txt
   # echo $angle 
   # echo $energy
 done < input_pt.txt 
